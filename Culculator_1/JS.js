1fa80567e6823d49dd149851166bf2036ab7d6a4
Rajesh <saini222302@gmail.com>
2022-08-04 21:40:05 +0530

    Calculator

--- Culculator_1/JS.js
+++ Culculator_1/JS.js
@@ -1,18 +1,37 @@
-let display = document.getElementById("display")
-let buttons = Array.from(document.getElementsByClassName("btn"))
-console.log(buttons)
-buttons.map((btn) =>{
-    btn.addEventListener("click" , (event) =>{
-        let key = event.target.innerText;
-        if(key=="A/C"){
-            display.innerText=""
-        }else if (key =="Delete"){
-            display.innerText.slice(0,-1)
-        } else if (key == "="){
-            display.innerText = eval(display.innerText) 
-        } else {
-            
-        }
+let display = document.getElementById("display");
 
-})
-})
+let buttons = Array.from(document.getElementsByClassName("btn"));
+
+buttons.map((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event);
+    let key = event.target.innerText;
+    if (key == "A/c") {
+      display.innerText = "";
+    } else if (key == "DEL") {
+      if (display.innerText == "Infinity") {
+        display.innerText = "";
+      } else {
+        display.innerText = display.innerText.slice(0, -1);
+      }
+    } else if (key == "=") {
+      if (display.innerText) {
+        display.innerText = eval(display.innerText);
+      }
+    } else if(key == "π"){
+        display.innerText += 3.14
+    }else if (key == "√"){
+        display.innerText = Math.sqrt(display.innerText)
+    }else if (key == "X!"){
+        display.innerText= "false"
+    } else if (key == "x2"){
+        display.innerText= Math.pow(x2 ,2)
+    }
+    
+    
+    
+    else {
+      display.innerText += key;
+    }
+  });
+});
