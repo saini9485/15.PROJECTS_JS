481036fb4bb0738cbb14b196f4957a100de871c8
Rajesh <saini222302@gmail.com>
2022-08-05 03:58:56 +0530

    Calculator

--- Culculator_1/JS.js
+++ Culculator_1/JS.js
@@ -18,18 +18,32 @@ buttons.map((btn) => {
       if (display.innerText) {
         display.innerText = eval(display.innerText);
       }
-    } else if(key == "π"){
-        display.innerText += 3.14
-    }else if (key == "√"){
-        display.innerText = Math.sqrt(display.innerText)
-    }else if (key == "X!"){
-        display.innerText= "false"
-    } else if (key == "x2"){
-        display.innerText= Math.pow(x2 ,2)
-    }
+    } else if (key == "π") {
+      display.innerText += 3.14;
+    } else if (key == "√") {
+      display.innerText = Math.sqrt(display.innerText);
+    } else if (key == "X!") {
+       let nums = display.innerText
+        let f = 1;
+        for(i = 2; i <= n; i++)
+            f = f*i;
+        display.innerText = f;
     
-    
-    
+    } else if (key == "x2") {
+      display.innerText = display.innerText ** 2;
+    } else if (key == "log") {
+      display.innerText = Math.log(display.innerText);
+    } else if (key == "sin"){
+      display.innerText= Math.sin(display.innerText)
+    }else if (key == "tan"){
+      display.innerText = Math.tan(display.innerText)
+    }else if (key =="cos"){
+      display.innerText = Math.cos(display.innerText)
+    }else if (key =="e"){
+      display.innerText = 2.71(display.innerText)
+  }else if (key == "X^"){
+    display.innerText += "^"
+  }
     else {
       display.innerText += key;
     }
