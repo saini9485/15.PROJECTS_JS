5c38305fa1c887166bf540d712b0c957c106c0e5
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-07 05:23:21 +0530

    Light

---
+++ Light_Off_On/index.js
@@ -0,0 +1,13 @@
+function light(value)
+	{
+	var pic;
+		if(value == 0) 
+		{
+			pic="light off.jpeg";
+		}
+		else
+		{
+			pic="light on.jpeg";
+		}
+		document.getElementById('bulb').src=pic;
+	}
