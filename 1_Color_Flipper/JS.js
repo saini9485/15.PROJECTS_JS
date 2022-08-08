beff75442947bc23eabe72da1d4b84441b33e495
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-08 17:07:29 +0530

    color_Flipper

--- 1_Color_Flipper/JS.js
+++ 1_Color_Flipper/JS.js
@@ -1,16 +1,18 @@
 const color_code = document.getElementById("color-code");
 const button = document.getElementById("btn");
+// const range= document.getElementById("range");
+// const colorRange= document.getElementById("colorRange");
 const getColor = () => {
   // Hex color Code
   const randomNumber = Math.floor(Math.random() * 16777215);
   const randomCode = "#" + randomNumber.toString(16);
-//console.log(randomNumber,randomCode)
-//color will show on display otherwise it will print in console
+  //console.log(randomNumber,randomCode)
+  //color will show on display otherwise it will print in console
   document.body.style.backgroundColor = randomCode;
-  //for genrates random Hex Code 
- color_code.innerText = randomCode;
-// for automatic copy the value
- // navigator.clipboard.writeText(randomCode);
+  //for genrates random Hex Code
+  color_code.innerText = randomCode;
+  // for automatic copy the value
+  // navigator.clipboard.writeText(randomCode);
 };
 button.addEventListener("click", getColor);
 // it will be call first and print random color before clicking
