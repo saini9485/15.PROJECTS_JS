3f3cb92b1c65595073c9285bae8f7e72aed1dd84
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-09 15:32:32 +0530

    Counter_Application

---
+++ 2_Counter/JS.js
@@ -0,0 +1,17 @@
+let counter = 0;
+function increment() {
+  counter++;
+  //console.log(counter)
+  document.getElementById("counter").innerHTML = counter;
+}
+function decrement() {
+  counter--;
+  
+  //console.log(counter)
+  document.getElementById("counter").innerHTML = counter;
+}
+function reset() {
+    counter=0;
+    //console.log(counter)
+    document.getElementById("counter").innerHTML = counter;
+  }
